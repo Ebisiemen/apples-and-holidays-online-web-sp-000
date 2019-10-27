ab0b4cd5eda0bd4beb1eec31186a7c82b5435ded
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-27 22:07:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,27 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+  holiday.each do |holiday_event, supplies|
+    parsed_event = holiday_event.to_s.split('_').map {|word| word.capitalize}
+    
+    puts "  #{parsed_event.to_s.capitalize}: #{supplies.join(', ')}"
+   end 
+  end 
+end
+
+
+# def all_supplies_in_holidays(supplies_hash)
+#   holiday_supplies.each do |season, holidays|
+#     puts "#{season.capitalize}:"
+#     holidays.each do |holiday, supplies|
+#       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+#     end
+#   end
+# end
+
+
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -57,15 +78,6 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday|
-    puts "#{season.to_s.capitalize}:"
-  holiday.each do |holiday_event, supplies|
-    parsed_event = holiday_event.to_s.split('_').map{|word| word.capitalize}
-    
-    puts "  #{parsed_event.to_s.capitalize}: #{supplies.join(', ')}"
-   end 
-  end 
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
