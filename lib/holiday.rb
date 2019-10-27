8b2e9dbe83cba6eb2cb92854fd11831bf067248e
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-27 21:54:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
   holiday.each do |holiday_event, supplies|
-    holiday_event = holiday_event.to_s.split('_').collect{|word| word.capitalize}.join(' ')
-    puts "  #{holiday_event.to_s.capitalize}: #{supplies.join(', ')}"
+    parsed_event = holiday_event.to_s.split('_').collect{|word| word.capitalize}.join(' ')
+    puts "  #{parsed_event.to_s.capitalize}: #{supplies.join(', ')}"
    end 
   end 
 end
